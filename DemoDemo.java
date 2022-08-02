32572c26ab903b70ac1348e7dd8f275c87abeaf1
Shahzad Niazi <96726883+shahzadkhan25@users.noreply.github.com>
2022-08-02 11:35:52 +0500

    Create DemoDemo.java

---
+++ DemoDemo.java
@@ -0,0 +1,19 @@
+class DemoDemo {
+ public static void main(String args[])
+ {
+ boolean b;
+
+ b = false;
+ System.out.println("b is " + b);
+
+ b = true;
+ System.out.println("b is " + b);
+
+ if(b) System.out.println("This is executed.");
+ b = false;
+
+ if(b) System.out.println("This is not executed.");
+ System.out.println("32 > 35 is " + (32 > 35));
+
+ }
+}
